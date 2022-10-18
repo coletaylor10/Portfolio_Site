e97189ab0914926bad0c923e671598eb1328b075
coletaylor10 <cole10.taylor@gmail.com>
2022-10-18 23:20:16 +0100

    errortesting

--- components/About.jsx
+++ components/About.jsx
@@ -4,7 +4,7 @@ import AboutImg from "../public/assets/about.jpg";
 
 const About = () => {
   return (
-    <div id='about'  className=" w-full md:h-screen p-2 flex items-center py-16 ">
+    <div id="about"  className=" w-full md:h-screen p-2 flex items-center py-16 ">
       <div className=" max-w-[1240px] m-auto md:grid grid-cols-3 gap-8 ">
         <div className="w-full h-auto m-auto shadow-xl border-2 border-[#ffef0f] rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
           <Image src={AboutImg} className="rounded-xl" alt="/" />
@@ -15,7 +15,7 @@ const About = () => {
           </p>
           <h2 className="py-4">Who I Am</h2>
           <p className=" py-2 text-gray-50">
-            // I'm Built different
+            // Im Built different
           </p>
           <p className=" py-2 text-gray-50">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, sed
